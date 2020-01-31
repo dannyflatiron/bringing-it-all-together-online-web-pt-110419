9d2ff6a354285ed69776c1afcd86c8d030d3fd1f
Daniel Reina <dannyflatiron@gmail.com>
2020-01-31 02:04:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,5 +9,13 @@ attr_accessor :name, :breed, :id
   end
 
   def self.create_table
+    sql =  <<-SQL
+       CREATE TABLE IF NOT EXISTS songs (
+         id INTEGER PRIMARY KEY,
+         name TEXT,
+         album TEXT
+         )
+         SQL
+    DB[:conn].execute(sql)
   end
 end
