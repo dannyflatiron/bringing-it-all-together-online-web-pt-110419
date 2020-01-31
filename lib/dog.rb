162abce84373d1bcf86366e44f2ff4dfb593dbc7
Daniel Reina <dannyflatiron@gmail.com>
2020-01-31 01:49:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,7 @@
 class Dog
 
-attr_accessor :name, :breed
-attr_writer :id
+attr_accessor :name, :breed, :id
+attr_writer
   def initialize(name:, breed:, id: nil)
     @name = name
     @breed = breed
