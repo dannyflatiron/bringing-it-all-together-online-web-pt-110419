b8176d0a9ddc366b4a18b811099f89cf0183cf74
Daniel Reina <dannyflatiron@gmail.com>
2020-01-31 02:37:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -26,11 +26,11 @@ attr_accessor :name, :breed, :id
 
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade)
+      INSERT INTO students (name, breed)
       VALUES (?, ?)
     SQL
 
-    DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
   end
 end
